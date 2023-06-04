ed5cae2f24d862a8a120d820deb8d74ed8dba3d2
phurhard <phurhardeen@gmail.com>
2023-06-04 10:58:45 +0100

    Lights out :(, revamped 1-hbnb.js, working on 2-hbnb.js

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   const amenities = {};
+  //AMENITIES LISTING
   $('input[data-id]').click(function () {
     const dataId = $(this).attr('data-id');
     const dataName = $(this).attr('data-name');
@@ -15,4 +16,13 @@ $(document).ready(function () {
     });
     $('div.amenities h4').text(amenityList.join(', '));
   });
+  //API REQUESTING
+	$.get('http://0.0.0.0:5001/api/v1/status/', function(data, statusText){
+		if (data == 'OK' || statusText == 200) {
+			$('div#api_status').addClass('available');
+		}
+		else {
+			$('div#api_status').removeClass('available');
+		}
+		});
 });
